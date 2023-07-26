167ccbcd45ab9b63c8afb219be3b6e01d65774e7
Sayaka Vincent <sfv2010@hotmail.fr>
2023-07-26 16:02:15 +0200

    Add Link

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../../components/Banner/Banner";
 import Card from "../../components/Card/Card";
 import datas from "../../datas/housingAdsList.json";
 import banner from "../../assets/banner.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
@@ -11,12 +12,13 @@ const Home = () => {
             <main className="mainContainer">
                 <div className="mainBox">
                     {datas.map((data) => (
-                        <Card
-                            data={data}
-                            key={data.id}
-                            title={data.title}
-                            cover={data.cover}
-                        />
+                        <Link key={data.id} to={`/accommodation/${data.id}`}>
+                            <Card
+                                key={data.id}
+                                title={data.title}
+                                cover={data.cover}
+                            />
+                        </Link>
                     ))}
                 </div>
             </main>
