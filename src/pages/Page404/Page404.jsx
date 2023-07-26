d493029ffcde323a6a7738f0d112a89f5445d6a8
Sayaka Vincent <sfv2010@hotmail.fr>
2023-07-26 16:02:59 +0200

    Add Link

--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -1,8 +1,17 @@
+import "./Page404.css";
+import error from "../../assets/404.png";
+import { Link } from "react-router-dom";
 const Page404 = () => {
     return (
-        <>
-            <div>Error404</div>
-        </>
+        <div className="errorContainer">
+            <img src={error} alt="Erreur 404" className="errorImg" />
+            <h1 className="errorH1">
+                Oups! La page que vous demandez n'existe pas.
+            </h1>
+            <Link to="/">
+                <p className="errorP">Retourner sur la page d’accueil</p>
+            </Link>
+        </div>
     );
 };
 
