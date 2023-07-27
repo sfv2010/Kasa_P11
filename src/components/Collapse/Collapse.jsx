9fdbb5bdf7c492fdfb27512eee7db7c2226b3824
Sayaka Vincent <sfv2010@hotmail.fr>
2023-07-27 16:54:56 +0200

    Add useRef

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,38 +1,34 @@
 import "./Collapse.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-const Collapse = ({ id, title, description }) => {
+const Collapse = ({
+    id,
+    title,
+    description,
+    summaryBar,
+    summaryTitle,
+    detailsDesc,
+}) => {
     const [isAnimated, setIsAnimated] = useState(false);
+    const collapseRef = useRef(null);
     const handleToggleAnimation = () => {
         setIsAnimated(false); // reset the animation
-        void document.getElementById(`collapse-${id}`).offsetWidth; // for the animation to restart
+        void collapseRef.current.offsetWidth; // for the animation to restart
         setIsAnimated(true); //Restart animation
     };
     return (
         <details className="collapseContainer">
-            <summary className="collapseBar" onClick={handleToggleAnimation}>
-                <h2 className="collapseH2">{title}</h2>
+            <summary className={summaryBar} onClick={handleToggleAnimation}>
+                <h2 className={summaryTitle}>{title}</h2>
             </summary>
             <div
                 id={`collapse-${id}`}
-                className={`collapse ${isAnimated ? "animated" : ""}`}>
-                <h3 className="collapseH3">{description}</h3>
+                className={`collapse ${isAnimated ? "animated" : ""}`}
+                ref={collapseRef}>
+                <p className={detailsDesc}>{description}</p>
             </div>
         </details>
     );
 };
 
 export default Collapse;
-
-// const Collapse = ({ title, description }) => {
-//     return (
-//         <div className="collapseContainer">
-//             <div className="collapseBar">
-//                 <h2 className="collapseH2">{title}</h2>
-//             </div>
-//             <div className="collapse">
-//                 <h3 className="collapseH3">{description}</h3>
-//             </div>
-//         </div>
-//     );
-// };
